01f4305c7935c41c2c378179f01e578b8f8e5492
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2024-09-26 20:03:10 +0200

    Add files via upload

--- src/RawImage.java
+++ src/RawImage.java
@@ -1,43 +1,111 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.io.FileNotFoundException;
+import java.nio.ByteOrder;
 import java.nio.file.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class RawImage {
     public static void LoadImage() {
-        String fitxer_image = "03508649.1_512_512_2_0_12_0_0_0.raw";
-        int files = 512;
-        int columnes = 512;
-        int components = 1;
-        int bytes_sample = 1;
-        boolean isUnsigned = true;
-        String path = "./imatges/" + fitxer_image;
+        List<String> images = new ArrayList<>();
 
-        //byte[] rawData = Files.readAllBytes(Paths.get(path));
-        File arxiu = new File(path);
-        int[][][] matriuImg = montarMatriu(arxiu, files, columnes, components, bytes_sample, isUnsigned);
+        images.add("03508649.1_512_512_2_0_12_0_0_0.raw");
+        images.add("03508649.64_512_512_2_0_12_0_0_0");
+        images.add("aviris_yellowstone_f060925t01p00r12_sc00_cal.224_512_677_3_0_16_0_0_0");
+        images.add("Landsat_agriculture.6_1024_1024_2_0_16_0_0_0");
+        images.add("mamo_1.1_3576_2944_2_0_12_0_0_0");
+        images.add("n1_GRAY.1_2560_2048_1_0_8_0_0_0");
+        images.add("n1_RGB.3_2560_2048_1_0_8_0_0_0");
+
+        String file_image = images.getFirst();
+
+
+        String[] parameters_raw = file_image.split("\\.");
+        String[] parameters = parameters_raw[1].split("_");
+
+        int files = Integer.parseInt(parameters[1]);
+        int columnes = Integer.parseInt(parameters[2]);
+        int components = Integer.parseInt(parameters[0]);
+
+        int bytes_sample = 0;
+        boolean isUnsigned = switch (Integer.parseInt(parameters[3])) {
+            //1-> 8 bits
+            case 1 -> {
+                bytes_sample = 1;
+                yield true;
+            }
+            //2-> 16 bits unsigned
+            case 2 -> {
+                bytes_sample = 2; // 2 bytes = 16 bits
+                yield true;
+            }
+            //3-> 16 bits signed
+            case 3 -> {
+                bytes_sample = 2;
+                yield false;
+            }
+            default -> true;
+        };
+
+
+        String path = "./imatges/" + file_image;
+        File file = new File(path); // Crear un objecte File amb el path
+        int[][][] matrixImg = generateMatrix(file, files, columnes, components, bytes_sample, isUnsigned);
     }
 
-    public static int[][][] montarMatriu(File arxiu, int files, int columns, int components, int bytes_sample, boolean isUnsigned) {
-        int[][][] matriu = new int[components][files][columns];
+    //Format int es invalid ja que estem utilitzant valors d'entre 1-2 bytes, perdent molta cpacitat, --> preguntat Joan
+    public static int[][][] generateMatrix(File arxiu, int files, int columns, int components, int bytes_sample, boolean isUnsigned) {
+        int[][][] matriu = new int[components][files][columns]; // Crear una matriu de 3 dimensions
 
-        try (FileInputStream fis = new FileInputStream(arxiu)) {
-            byte[] buffer = new byte[files * columns * components * bytes_sample];
-            int byteRead = fis.read(buffer);
+        try (FileInputStream fis = new FileInputStream(arxiu)) { // Crear un objecte FileInputStream amb l'arxiu
+            byte[] buffer = new byte[files * columns * components * bytes_sample]; // Crear un buffer de bytes
+            int byteRead = fis.read(buffer); // Llegir els bytes de l'arxiu i guardar-los al buffer
 
-            if (byteRead != buffer.length) {
-                throw new IOException("Different file size");
+            if (byteRead != buffer.length) { // Comprovar que s'han llegit tots els bytes
+                throw new IOException("Different file size"); // Llençar una excepció si no s'han llegit tots els bytes
             }
 
-            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer); // Crear un objecte ByteBuffer amb el buffer
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN); // Establir el byte order del ByteBuffer, sempre ha de ser aquest ordre
 
+            int index = 0;
+            for (int i = 0; i < components; i++) {
+                for (int j = 0; j < files; j++) {
+                    for (int k = 0; k < columns; k++) {
+                        int value;
+                        if (bytes_sample == 1) {
+                            //Si es un byte s'utilitza el format byte, ja que ocupa menys que int
+                            byte b = byteBuffer.get(index);
+                            if (isUnsigned) {
+                                value = Byte.toUnsignedInt(b); //Si es unsigned, convertim al rang [0,255]
+                            } else {
+                                value = b; // Si es signed, Java ho interpreta correctament en [-128, 127] -->perque?? pregunta joan
+                            }
+                        } else if (bytes_sample == 2) {
+                            //Si son 2 bytes es un short
+                            short s = byteBuffer.getShort(index);
+                            if (isUnsigned) {
+                                value = Short.toUnsignedInt(s); //Si es unsigned, convertim al rang [0,65535]
+                            } else {
+                                value = s; //Si es signed, ja està al rang correcte [-32768, 32767]
+                            }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                            matriu[i][j][k] = value;
+                            index += bytes_sample;
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException e) { // Capturar una excepció d'entrada/sortida
+            throw new RuntimeException(e); // Llençar una nova excepció
         }
         return matriu;
     }
